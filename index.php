0d463970c11ed3f2116f78127c25a78d284b72d2
Zegert Boele <83504@glr.nl>
2021-02-06 16:23:04 +0100

    styling kinda works, time for Php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ head("Zegert Boele || Webdevelopment");
 <body>
     <div class="container">
         <div class="landingpage">
-
             <div class="row">
                 <h1 class="maintitle">Zegert Boele</h1>
             </div>
@@ -17,44 +16,187 @@ head("Zegert Boele || Webdevelopment");
                 <h3 class="subtitle">Programming, Coffee, Sleep, Repeat</h2>
             </div>
         </div>
+        <hr>
         <div class="row">
             <div class="col">
-                <h1>Projects</h1>
+                <h1 class="H1">About Me</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <p class="aboutmeText">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quod obcaecati facilis nihil
+                    alias quia, doloribus accusantium aliquid. Dolore non alias animi iusto recusandae corporis maxime
+                    expedita delectus minus eius!<br>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nihil illo commodi, dolores
+                    praesentium nisi consectetur perferendis eveniet nam fuga accusantium dolorum quis similique
+                    delectus, blanditiis corporis. Consequuntur, aperiam corporis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore hic id iste at? Nostrum, at quo
+                    odio vero doloremque hic ea eveniet placeat libero error tempora, voluptatem ullam ipsam sit.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid distinctio, dolores modi
+                    laboriosam sunt quis aut exercitationem autem. Ipsum, illo. Eum nulla sed nihil nostrum nam
+                    voluptatibus est ipsum explicabo.<br>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eius doloribus animi officia quia
+                    nihil repudiandae voluptate, minima id debitis fuga sint quod, quis ut ipsam tenetur in accusamus
+                    harum.<br><br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aliquam totam doloribus voluptates!
+                    Necessitatibus nostrum provident, odio officia culpa pariatur consectetur placeat rerum quia aut
+                    enim iste minus vel optio!<br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam debitis rerum vel nulla nobis natus
+                    adipisci laudantium velit. Itaque repellat ea ab placeat dignissimos, magni quasi eaque? Mollitia,
+                    quam quos.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellat placeat ab impedit itaque
+                    doloremque, a magni quidem facilis natus sed reprehenderit illum, quia dolorem repudiandae tenetur,
+                    excepturi consequuntur qui!praesentium nisi consectetur perferendis eveniet nam fuga accusantium
+                    dolorum quis similique
+                    delectus, blanditiis corporis. Consequuntur, aperiam corporis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore hic id iste at? Nostrum, at quo
+                    odio vero doloremque hic ea eveniet placeat libero error tempora, voluptatem ullam ipsam sit.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid distinctio, dolores modi
+                    laboriosam sunt quis aut exercitationem autem. Ipsum, illo. Eum nulla sed nihil nostrum nam
+                    voluptatibus est ipsum explicabo.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eius doloribus animi officia quia
+                    nihil repudiandae voluptate, minima id debitis fuga sint quod, quis ut ipsam tenetur in accusamus
+                    harum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aliquam totam doloribus voluptates!
+                    Necessitatibus nostrum provident, odio officia culpa pariatur consectetur placeat rerum quia aut
+                    enim iste minus vel optio!
+                </p>
+            </div>
+            <div class="col-4">
+                <img class="aboutmeImg" src="./Includes/IMG/aboutme.jpeg" alt="A photo of me.">
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col">
+                <h1 class="H1">Programming languages</h1>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/python.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Python
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/html.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            HTML 5
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/css.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            CSS 3
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/php.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            PHP 8.0
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/python.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Python
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-secondary text-center programLangCard">
+                    <img class="card-img-top" src="./Includes/IMG/PlIcons/python.png">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Python
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col">
+                <h1 class="H1">Projects</h1>
             </div>
         </div>
         <div class="row justify-content-center">
             <div class="col portfolio-item">
-                <a href="project-page.php?ID=">
-                    <div class="card">
-                        <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
-                        <h4 class="card-title">Item 1</h4>
-                        <p class="card-text">lorem ipsum dolor is amet</p>
+                <div class="card text-white bg-secondary">
+                    <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">This is an awesome card used for showing</h4>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui itaque aliquam nulla quaerat
+                            expedita neque! Dicta, molestias natus placeat quaerat, dignissimos adipisci dolore
+                            voluptatibus id repellat neque maiores possimus! Necessitatibus?
+                        </p>
                     </div>
-            </div>
-            </a>
-            <div class="col portfolio-item">
-                <div class="card">
-                    <a href="project-page.php?ID=">
-                        <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
-                        <h4 class="card-title">Item 2</h4>
-                        <p class="card-text">lorem ipsum dolor is amet</p>
-                    </a>
+                    <div class="card-body">
+                        <a href="project-page.php?ID=" class="btn btn-secondary">Details</a>
+                    </div>
                 </div>
             </div>
             <div class="col portfolio-item">
-                <div class="card">
-                    <a href="project-page.php?ID=">
-                        <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
-                        <h4 class="card-title">Item 3</h4>
-                        <p class="card-text">lorem ipsum dolor is amet</p>
-                    </a>
+                <div class="card text-white bg-secondary">
+                    <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">This is an awesome card used for showing</h4>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui itaque aliquam nulla quaerat
+                            expedita neque! Dicta, molestias natus placeat quaerat, dignissimos adipisci dolore
+                            voluptatibus id repellat neque maiores possimus! Necessitatibus?
+                        </p>
+                    </div>
+                    <div class="card-body">
+                        <a href="project-page.php?ID=" class="btn btn-secondary">Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col portfolio-item">
+                <div class="card text-white bg-secondary">
+                    <img class="card-img-top" src="./Includes/IMG/img1.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">This is an awesome card used for showing</h4>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui itaque aliquam nulla quaerat
+                            expedita neque! Dicta, molestias natus placeat quaerat, dignissimos adipisci dolore
+                            voluptatibus id repellat neque maiores possimus! Necessitatibus?
+                        </p>
+                    </div>
+                    <div class="card-body">
+                        <a href="project-page.php?ID=" class="btn btn-secondary">Details</a>
+                    </div>
                 </div>
             </div>
         </div>
+        <hr>
         <form action="" method="POST">
             <div class="row">
                 <div class="col">
-                    <h1>Contact</h1>
+                    <h1 class="H1">Contact</h1>
                 </div>
             </div>
             <div class="row">
